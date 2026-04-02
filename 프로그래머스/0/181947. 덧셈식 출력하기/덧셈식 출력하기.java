344f25bb186d0c385b8764c329af1d2e5618247c
kimjaeyun124 <kimjaeyun124@gmail.com>
2026-04-02 19:54:55 +0900

    [level 0] Title: 덧셈식 출력하기, Time: 268.34 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/0/181947. 덧셈식 출력하기/덧셈식 출력하기.java
+++ 프로그래머스/0/181947. 덧셈식 출력하기/덧셈식 출력하기.java
@@ -5,6 +5,6 @@ public class Solution {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        System.out.println(a + " + " + b + " = " + (a + b));
+        System.out.printf("%d + %d = %d", a, b, a + b);
     }
 }
